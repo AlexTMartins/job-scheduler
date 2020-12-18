07ed4ae258ad17dff6c5b6a91b192b1abaa16807
Alex <Alex@KAHENA.mshome.net>
2020-12-18 02:45:15 -0200

    start grouping jobs by max hours to execute.

--- src/main/java/com/alex/jobScheduler/service/JobSchedulerService.java
+++ src/main/java/com/alex/jobScheduler/service/JobSchedulerService.java
@@ -1,5 +1,6 @@
 package com.alex.jobScheduler.service;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,14 +14,39 @@ public class JobSchedulerService {
 	public List<List<Job>> scheduleJobs(List<Job> jobs) {
 			
 		sortJobsByMaxDate(jobs);
+		return groupJobsByLimitTime(jobs, getMaxHoursToExecute());		
+	}
+	
+	public List<List<Job>> groupJobsByLimitTime(List<Job> jobs, int limitTimeInHours){
+	
+		List<List<Job>> groupedJobs = new ArrayList<List<Job>>();
 		
-		return null;
+		analyzeJobsToGroup(groupedJobs,jobs, limitTimeInHours);
+		
+		return groupedJobs;
+	}
+	
+	public void analyzeJobsToGroup(List<List<Job>> groupedJobs, List<Job> jobs, int limitTimeInHours) {
+
+		List notGroupedJobs = new ArrayList<String>();
+		List jobsToGroup = new ArrayList<String>();
+		
+		int timeCounter = 0; 
+		
+		jobs.forEach((job) -> {
+			if(job.getTempoEstimado() + timeCounter <= limitTimeInHours) {
+				jobsToGroup.add(job);
+			}
+			
+        });
+		
 	}
 	
 	public void sortJobsByMaxDate(List<Job> jobs) {
-		
 		jobs.sort(Comparator.comparing(Job::getDataMaximaDeConclusao));	
 	}
 	
-	
+	private int getMaxHoursToExecute(){
+		return 8;
+	}
 }
